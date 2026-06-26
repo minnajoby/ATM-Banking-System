9a04c88201dcbe7acd92b7a3d6feef2e67c0974d
Minna Joby <minnajoby27@gmail.com>
2026-06-26 09:10:27 +0530

    Implement Account Creation Module

--- db_connection.py
+++ db_connection.py
@@ -1,9 +1,10 @@
 import pymysql
-conn = pymysql.connect(host = 'localhost',user = 'root',password = input("Enter DB Password: "),database = 'atm_system')
-cur = conn.cursor()
-cur.execute('select * from accounts')
-conn.commit()
-res = cur.fetchmany(size = 10)
-for col in res:
-    print(col)
-conn.close()
+
+def get_connection():
+    conn = pymysql.connect(
+        host = 'localhost',
+        user = 'root',
+        password = input("Enter DB Password: "),
+        database = 'atm_system'
+    )
+    return conn
