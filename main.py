ff1574a1861bd0298579fa609020b309b8b9b3bd
Minna Joby <minnajoby27@gmail.com>
2026-06-27 19:15:52 +0530

    Implement main menu system

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-
-
+while True:
+    print("===== ATM BANKING SYSTEM =====\n")
+    print("1.Create Account\n2.Login\n3.Deposit\n4.Withdraw\n5.Balance Enquiry\n6.Exit\n")
+    choice = int(input("Enter Your Choice:"))
+    if choice == 1:
+        print("Create Account selected")
+    elif choice == 2:
+        print("Login selected")
+    elif choice == 3:
+        print("Deposit selected")
+    elif choice == 4:
+        print("Withdraw selected")
+    elif choice == 5:
+        print("Balance selected")
+    elif choice == 6:
+        break
+    else:
+        print("Invalid Choice")
