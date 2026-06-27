a81b67002518d296889998d82a4e5701307b3e52
Minna Joby <minnajoby27@gmail.com>
2026-06-27 19:44:05 +0530

    Improve account creation validation flow

--- create_account.py
+++ create_account.py
@@ -3,14 +3,14 @@ def create_account():
     conn = get_connection()
     cur = conn.cursor()
     create_acc_no = int(input('Enter Account Number:'))
-    create_name = input('Enter Name:')
-    create_pin = int(input('Enter PIN:'))
-    create_balance = float(input('Enter Initial Deposit:'))
     cur.execute('select acc_no from accounts where acc_no = %s',(create_acc_no,))
     result = cur.fetchone()
     if result is not None:
         print("Account Already exists")
     else:
+        create_name = input('Enter Name:')
+        create_pin = int(input('Enter PIN:'))
+        create_balance = float(input('Enter Initial Deposit:'))
         cur.execute('insert into accounts (acc_no,name,pin,balance) values(%s,%s,%s,%s)',(create_acc_no,create_name,create_pin,create_balance,))
         conn.commit()
         print('Account Created Successfully')
